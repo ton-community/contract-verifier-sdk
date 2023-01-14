f83c0846b9e1a8a126b9ab268d438c0079aea81d
Shahar Yakir <shahar.yakir@gmail.com>
2023-01-14 15:55:25 +0200

    fixed safari

--- build.js
+++ build.js
@@ -31,7 +31,10 @@ const { polyfillNode } = require("esbuild-plugin-polyfill-node");
       target: "ES2020",
       outfile: "./dist/index.min.js",
       sourcemap: true,
-      minify: false,
+      minify: true,
+      supported: {
+        "regexp-lookbehind-assertions": false
+      }
     })
     .catch(() => process.exit(1));
 })();
