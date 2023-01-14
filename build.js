b28070ee467aafb252af33c07371965415ccd8b6
Shahar Yakir <shahar.yakir@gmail.com>
2023-01-14 16:20:36 +0200

    in prog

--- build.js
+++ build.js
@@ -32,9 +32,9 @@ const { polyfillNode } = require("esbuild-plugin-polyfill-node");
       outfile: "./dist/index.min.js",
       sourcemap: true,
       minify: true,
-      supported: {
-        "regexp-lookbehind-assertions": false
-      }
+      // supported: {
+      //   "regexp-lookbehind-assertions": false
+      // }
     })
     .catch(() => process.exit(1));
 })();
