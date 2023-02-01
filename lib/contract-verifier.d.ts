b5314ab6497d6ddaa1383a7ae182d5a9a93866e2
Shahar Yakir <shahar.yakir@gmail.com>
2023-02-01 11:16:19 +0200

    Func041 (#89)
    * func041
    
    * 0.3.0

--- lib/contract-verifier.d.ts
+++ lib/contract-verifier.d.ts
@@ -3,7 +3,7 @@ interface GetSourcesOptions {
     httpApiEndpoint?: string;
     httpApiKey?: string;
 }
-export declare type FuncCompilerVersion = "0.2.0" | "0.3.0" | "0.4.0";
+export declare type FuncCompilerVersion = "0.2.0" | "0.3.0" | "0.4.0" | "0.4.1";
 export declare type TactVersion = "0.4.0";
 export declare type FiftVersion = FuncCompilerVersion;
 export declare type FuncCompilerSettings = {
